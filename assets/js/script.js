8d1f25da999ff716af7c12ac8fc513f2093cea7b
ailalhamdibasri <ailalhamdibasri5@gmail.com>
2023-10-04 20:43:09 +0800

    frontend

---
+++ assets/js/script.js
@@ -0,0 +1,112 @@
+'use strict';
+
+/**
+ * navbar toggle
+ */
+
+const overlay = document.querySelector("[data-overlay]");
+const navOpenBtn = document.querySelector("[data-nav-open-btn]");
+const navbar = document.querySelector("[data-navbar]");
+const navCloseBtn = document.querySelector("[data-nav-close-btn]");
+const navLinks = document.querySelectorAll("[data-nav-link]");
+
+const navElemArr = [navOpenBtn, navCloseBtn, overlay];
+
+const navToggleEvent = function (elem) {
+  for (let i = 0; i < elem.length; i++) {
+    elem[i].addEventListener("click", function () {
+      navbar.classList.toggle("active");
+      overlay.classList.toggle("active");
+    });
+  }
+}
+
+navToggleEvent(navElemArr);
+navToggleEvent(navLinks);
+
+/**
+ * header sticky & go to top
+ */
+
+const header = document.querySelector("[data-header]");
+const goTopBtn = document.querySelector("[data-go-top]");
+
+window.addEventListener("scroll", function () {
+
+  if (window.scrollY >= 200) {
+    header.classList.add("active");
+    goTopBtn.classList.add("active");
+  } else {
+    header.classList.remove("active");
+    goTopBtn.classList.remove("active");
+  }
+
+});
+
+function redirectToMoreInfo() {
+  // Ganti URL dengan halaman tujuan yang sesuai
+  window.location.href = 'more_info_page.html';
+}
+
+const images = [...document.querySelectorAll('.imagess')];
+
+// popup
+
+const popup = document.querySelector('.popup');
+const closeBtn = document.querySelector('.close-btn');
+const imageName = document.querySelector('.image-name');
+const largeImage = document.querySelector('.large-image');
+const imageIndex = document.querySelector('.index');
+const leftArrow = document.querySelector('.left-arrow');
+const rightArrow = document.querySelector('.right-arrow');
+
+let index = 0; // will track our current image;
+
+images.forEach((item, i) => {
+    item.addEventListener('click', () => {
+        updateImage(i);
+        popup.classList.toggle('active');
+    })
+})
+
+const updateImage = (i) => {
+    let path = `assets/images/testi${i+1}.jpeg`;
+    largeImage.src = path;
+    imageName.innerHTML = `assets/images/testi${i+1}.jpeg`;
+    imageIndex.innerHTML = `0${i+1}`;
+    index = i;
+}
+
+closeBtn.addEventListener('click', () => {
+    popup.classList.toggle('active');
+})
+
+leftArrow.addEventListener('click', () => {
+    if(index > 0){
+        updateImage(index - 1);
+    }
+})
+
+rightArrow.addEventListener('click', () => {
+    if(index < images.length - 1){
+        updateImage(index + 1);
+    }
+})
+
+let popupcontainer = document.getElementById('popupmore');
+
+function openPopup(){
+  popupcontainer.classList.add("open-popup");
+}
+
+function closePopup(){
+  popupcontainer.classList.remove("open-popup");
+}
+
+function redirectToWhatsApp() {
+  window.location.href = "https://wa.me/6285377955384";
+}
+
+function scrollToFooter() {
+  document.getElementById('contact').scrollIntoView({ behavior: 'smooth' });
+}
